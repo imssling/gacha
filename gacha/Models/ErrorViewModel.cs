0544154fd88f2a06cccea8f30056131c3302f22d
Serene <lingling90526@gmail.com>
2024-06-27 11:23:16 +0800

    加入專案檔案。

---
+++ gacha/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace gacha.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ gacha/Partials/gachaContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace gacha.Models
+{
+    public partial class gachaContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                IConfiguration Config = new ConfigurationBuilder()
+                    .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+                optionsBuilder.UseSqlServer(Config.GetConnectionString("gacha"));
+            }
+        }
+    }
+}
