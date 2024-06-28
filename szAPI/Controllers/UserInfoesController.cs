e1061ecc6898d0445450e2a3024260414e1fb803
Johnson <oscar5566101@gmail.com>
2024-06-28 11:28:27 +0800

    可以找所有會員資料的API

---
+++ szAPI/Controllers/UserInfoesController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Azure.Identity;
+using Humanizer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages.Manage;
+using szAPI.DTO;
+using szAPI.Models;
+
+namespace szAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserInfoesController : ControllerBase
+    {
+        private readonly gachaContext _context;
+
+        public UserInfoesController(gachaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/UserInfoes
+        [HttpGet]
+        public async Task<IEnumerable<UserInfoDTO>> GetUserInfos()
+        {
+            return _context.UserInfos.Select(UserInfos => new UserInfoDTO
+            {
+                id = UserInfos.Id,
+                userName = UserInfos.UserName,
+                phoneNumber = UserInfos.PhoneNumber,
+                email = UserInfos.Email,
+                address = UserInfos.Address,
+                gender = UserInfos.Gender,
+            });
+        }
+
+        // GET: api/UserInfoes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserInfo>> GetUserInfo(int id)
+        {
+            var userInfo = await _context.UserInfos.FindAsync(id);
+
+            if (userInfo == null)
+            {
+                return NotFound();
+            }
+
+            return userInfo;
+        }
+
+        // PUT: api/UserInfoes/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUserInfo(int id, UserInfo userInfo)
+        {
+            if (id != userInfo.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(userInfo).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserInfoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/UserInfoes
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<UserInfo>> PostUserInfo(UserInfo userInfo)
+        {
+            _context.UserInfos.Add(userInfo);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUserInfo", new { id = userInfo.Id }, userInfo);
+        }
+
+        // DELETE: api/UserInfoes/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUserInfo(int id)
+        {
+            var userInfo = await _context.UserInfos.FindAsync(id);
+            if (userInfo == null)
+            {
+                return NotFound();
+            }
+
+            _context.UserInfos.Remove(userInfo);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool UserInfoExists(int id)
+        {
+            return _context.UserInfos.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ szAPI/DTO/UserInfoDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace szAPI.DTO
+{
+    public class UserInfoDTO
+    {
+        public int id { get; set; }
+        public string? userName { get; set; }
+        public string? phoneNumber { get; set; }
+        public string? email { get; set; }
+        public string? address { get; set; }
+        public string? gender { get; set; }
+    }
+}
