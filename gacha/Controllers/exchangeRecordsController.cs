1f4110cab2a7f995171a93ee2aa7fab845e80201
LiYang <liyangfullstack8776@gmail.com>
2024-06-28 18:47:55 +0800

    新增: exchangRecord Controller/permission Controller/pointList Controller/trackingList Controller 問題: trackingList index 找不到 ID

---
+++ gacha/Controllers/exchangeRecordsController.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using gacha.Models;
+
+namespace gacha.Controllers
+{
+    public class exchangeRecordsController : Controller
+    {
+        private readonly gachaContext _context;
+
+        public exchangeRecordsController(gachaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: exchangeRecords
+        public async Task<IActionResult> Index()
+        {
+            var gachaContext = _context.exchangeRecord.Include(e => e.gachaIdFromNavigation).Include(e => e.gachaIdToNavigation).Include(e => e.userIdFromNavigation).Include(e => e.userIdToNavigation);
+            return View(await gachaContext.ToListAsync());
+        }
+
+        // GET: exchangeRecords/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var exchangeRecord = await _context.exchangeRecord
+                .Include(e => e.gachaIdFromNavigation)
+                .Include(e => e.gachaIdToNavigation)
+                .Include(e => e.userIdFromNavigation)
+                .Include(e => e.userIdToNavigation)
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (exchangeRecord == null)
+            {
+                return NotFound();
+            }
+
+            return View(exchangeRecord);
+        }
+
+        // GET: exchangeRecords/Create
+        public IActionResult Create()
+        {
+            ViewData["gachaIdFrom"] = new SelectList(_context.gachaProduct, "id", "productName");
+            ViewData["gachaIdTo"] = new SelectList(_context.gachaProduct, "id", "productName");
+            ViewData["userIdFrom"] = new SelectList(_context.userInfo, "id", "email");
+            ViewData["userIdTo"] = new SelectList(_context.userInfo, "id", "email");
+            return View();
+        }
+
+        // POST: exchangeRecords/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("id,userIdFrom,userIdTo,gachaIdFrom,gachaIdTo,exchangeDate")] exchangeRecord exchangeRecord)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(exchangeRecord);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["gachaIdFrom"] = new SelectList(_context.gachaProduct, "id", "productName", exchangeRecord.gachaIdFrom);
+            ViewData["gachaIdTo"] = new SelectList(_context.gachaProduct, "id", "productName", exchangeRecord.gachaIdTo);
+            ViewData["userIdFrom"] = new SelectList(_context.userInfo, "id", "email", exchangeRecord.userIdFrom);
+            ViewData["userIdTo"] = new SelectList(_context.userInfo, "id", "email", exchangeRecord.userIdTo);
+            return View(exchangeRecord);
+        }
+
+        // GET: exchangeRecords/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var exchangeRecord = await _context.exchangeRecord.FindAsync(id);
+            if (exchangeRecord == null)
+            {
+                return NotFound();
+            }
+            ViewData["gachaIdFrom"] = new SelectList(_context.gachaProduct, "id", "productName", exchangeRecord.gachaIdFrom);
+            ViewData["gachaIdTo"] = new SelectList(_context.gachaProduct, "id", "productName", exchangeRecord.gachaIdTo);
+            ViewData["userIdFrom"] = new SelectList(_context.userInfo, "id", "email", exchangeRecord.userIdFrom);
+            ViewData["userIdTo"] = new SelectList(_context.userInfo, "id", "email", exchangeRecord.userIdTo);
+            return View(exchangeRecord);
+        }
+
+        // POST: exchangeRecords/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("id,userIdFrom,userIdTo,gachaIdFrom,gachaIdTo,exchangeDate")] exchangeRecord exchangeRecord)
+        {
+            if (id != exchangeRecord.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(exchangeRecord);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!exchangeRecordExists(exchangeRecord.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["gachaIdFrom"] = new SelectList(_context.gachaProduct, "id", "productName", exchangeRecord.gachaIdFrom);
+            ViewData["gachaIdTo"] = new SelectList(_context.gachaProduct, "id", "productName", exchangeRecord.gachaIdTo);
+            ViewData["userIdFrom"] = new SelectList(_context.userInfo, "id", "email", exchangeRecord.userIdFrom);
+            ViewData["userIdTo"] = new SelectList(_context.userInfo, "id", "email", exchangeRecord.userIdTo);
+            return View(exchangeRecord);
+        }
+
+        // GET: exchangeRecords/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var exchangeRecord = await _context.exchangeRecord
+                .Include(e => e.gachaIdFromNavigation)
+                .Include(e => e.gachaIdToNavigation)
+                .Include(e => e.userIdFromNavigation)
+                .Include(e => e.userIdToNavigation)
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (exchangeRecord == null)
+            {
+                return NotFound();
+            }
+
+            return View(exchangeRecord);
+        }
+
+        // POST: exchangeRecords/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var exchangeRecord = await _context.exchangeRecord.FindAsync(id);
+            if (exchangeRecord != null)
+            {
+                _context.exchangeRecord.Remove(exchangeRecord);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool exchangeRecordExists(int id)
+        {
+            return _context.exchangeRecord.Any(e => e.id == id);
+        }
+    }
+}
---
+++ gacha/Controllers/pointListsController.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using gacha.Models;
+
+namespace gacha.Controllers
+{
+    public class pointListsController : Controller
+    {
+        private readonly gachaContext _context;
+
+        public pointListsController(gachaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: pointLists
+        public async Task<IActionResult> Index()
+        {
+            var gachaContext = _context.pointList.Include(p => p.achievement).Include(p => p.bag).Include(p => p.rechargeList);
+            return View(await gachaContext.ToListAsync());
+        }
+
+        // GET: pointLists/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pointList = await _context.pointList
+                .Include(p => p.achievement)
+                .Include(p => p.bag)
+                .Include(p => p.rechargeList)
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (pointList == null)
+            {
+                return NotFound();
+            }
+
+            return View(pointList);
+        }
+
+        // GET: pointLists/Create
+        public IActionResult Create()
+        {
+            ViewData["achievementId"] = new SelectList(_context.achievement, "id", "name");
+            ViewData["bagId"] = new SelectList(_context.bag, "id", "gachaStatus");
+            ViewData["rechargeListId"] = new SelectList(_context.rechargeList, "id", "paymentMode");
+            return View();
+        }
+
+        // POST: pointLists/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("id,rechargeListId,bagId,achievementId")] pointList pointList)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(pointList);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["achievementId"] = new SelectList(_context.achievement, "id", "name", pointList.achievementId);
+            ViewData["bagId"] = new SelectList(_context.bag, "id", "gachaStatus", pointList.bagId);
+            ViewData["rechargeListId"] = new SelectList(_context.rechargeList, "id", "paymentMode", pointList.rechargeListId);
+            return View(pointList);
+        }
+
+        // GET: pointLists/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pointList = await _context.pointList.FindAsync(id);
+            if (pointList == null)
+            {
+                return NotFound();
+            }
+            ViewData["achievementId"] = new SelectList(_context.achievement, "id", "name", pointList.achievementId);
+            ViewData["bagId"] = new SelectList(_context.bag, "id", "gachaStatus", pointList.bagId);
+            ViewData["rechargeListId"] = new SelectList(_context.rechargeList, "id", "paymentMode", pointList.rechargeListId);
+            return View(pointList);
+        }
+
+        // POST: pointLists/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("id,rechargeListId,bagId,achievementId")] pointList pointList)
+        {
+            if (id != pointList.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(pointList);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!pointListExists(pointList.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["achievementId"] = new SelectList(_context.achievement, "id", "name", pointList.achievementId);
+            ViewData["bagId"] = new SelectList(_context.bag, "id", "gachaStatus", pointList.bagId);
+            ViewData["rechargeListId"] = new SelectList(_context.rechargeList, "id", "paymentMode", pointList.rechargeListId);
+            return View(pointList);
+        }
+
+        // GET: pointLists/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pointList = await _context.pointList
+                .Include(p => p.achievement)
+                .Include(p => p.bag)
+                .Include(p => p.rechargeList)
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (pointList == null)
+            {
+                return NotFound();
+            }
+
+            return View(pointList);
+        }
+
+        // POST: pointLists/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var pointList = await _context.pointList.FindAsync(id);
+            if (pointList != null)
+            {
+                _context.pointList.Remove(pointList);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool pointListExists(int id)
+        {
+            return _context.pointList.Any(e => e.id == id);
+        }
+    }
+}
