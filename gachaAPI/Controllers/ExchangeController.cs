91f49f8ea65f9c5d21e88cc3f2dc9f6ed24c232d
Allen <a25580819a1107@gmail.com>
2024-07-05 16:36:07 +0800

    create ExangeController update UploadController

---
+++ gachaAPI/Controllers/ExchangeController.cs
@@ -0,0 +1,72 @@
+﻿using gachaAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace gachaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExchangeController : ControllerBase
+    {
+
+        private readonly gachaContext _context;
+
+        public ExchangeController(gachaContext context)
+        {
+            _context = context;
+        }
+
+
+
+        // GET api/<ExchangeController>/5
+        [HttpGet("GetUserName/{id}")]
+        public async Task<string> GetUserName(int id)
+        {
+            UserInfo user = await _context.UserInfos.FindAsync(id);
+
+            if (user == null)
+            {
+                return "錯誤! 找不到指定用戶!";
+            }
+
+            else
+            {
+                return $"{user.UserName}";
+            }
+
+        }
+
+        // POST api/<ExchangeController>
+        [HttpPost("ExchangeConfirm")]
+        public async Task<string> ExchangeConfirm(bool isUserFromConfirm, bool isUserToConfirm, int intUserIdFrom, int intUserIdTo, int intGachaIdFrom, int intGachaIdTo)
+        {
+            if (isUserFromConfirm && isUserToConfirm)
+            {
+                ExchangeRecord exchangeRecord = new ExchangeRecord() 
+                {
+                    UserIdFrom = intUserIdFrom,
+                    UserIdTo = intUserIdTo,
+                    GachaIdFrom = intGachaIdTo,
+                    GachaIdTo = intGachaIdTo
+                };
+
+                _context.ExchangeRecords.Add(exchangeRecord);
+                await _context.SaveChangesAsync();
+
+                return $"新增交換紀錄成功! ID : {exchangeRecord.Id}";
+            }
+
+            else
+            {
+                return $"新增交換紀錄失敗!";
+            }
+
+
+        }
+
+  
+    }
+}
---
+++ gachaAPI/Controllers/UploadController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using gachaAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace gachaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+        private readonly gachaContext _context;
+
+        public UploadController(gachaContext context)
+        {
+            _context = context;
+        }
+
+
+
+
+        // GET: api/<UploadController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<UploadController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<UploadController>
+        [HttpPost]
+        public async Task<string> Upload(int intBagId)
+        {
+            var bag = await _context.Bags.FindAsync(intBagId);
+
+            if (bag == null)
+            {
+                return "上架失敗!";
+            }
+
+            else
+            {
+                bag.GachaStatus = "已上架";
+
+
+                _context.Entry(bag).State = EntityState.Modified;
+
+                try
+                {
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BagExists(intBagId))
+                    {
+                        return "上架失敗!";
+                    }
+                    else
+                    {
+                        return "上架失敗!";
+                    }
+                }
+
+
+
+                return "上架成功!";
+            }
+
+        }
+
+        private bool BagExists(int id)
+        {
+            return _context.Bags.Any(e => e.Id == id);
+        }
+
+        // PUT api/<UploadController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<UploadController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
