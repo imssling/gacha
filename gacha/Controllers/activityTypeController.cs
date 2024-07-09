fb9eab07d76a1a62d93f077e55348ece8a254196
LiYang <liyangfullstack8776@gmail.com>
2024-07-09 19:49:25 +0800

    新增:  activityType: controller/partial/Metadata
     修改:
    中文化 crud at trackingling/shippingpermission/exchangeRecord

---
+++ gacha/Controllers/activityTypeController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using gacha.Models;
+
+namespace gacha.Controllers
+{
+    public class activityTypeController : Controller
+    {
+        private readonly gachaContext _context;
+
+        public activityTypeController(gachaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: activityType
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.activityType.ToListAsync());
+        }
+
+        // GET: activityType/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var activityType = await _context.activityType
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (activityType == null)
+            {
+                return NotFound();
+            }
+
+            return View(activityType);
+        }
+
+        // GET: activityType/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: activityType/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("id,name,createdAt")] activityType activityType)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(activityType);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(activityType);
+        }
+
+        // GET: activityType/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var activityType = await _context.activityType.FindAsync(id);
+            if (activityType == null)
+            {
+                return NotFound();
+            }
+            return View(activityType);
+        }
+
+        // POST: activityType/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("id,name,createdAt")] activityType activityType)
+        {
+            if (id != activityType.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(activityType);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!activityTypeExists(activityType.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(activityType);
+        }
+
+        // GET: activityType/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var activityType = await _context.activityType
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (activityType == null)
+            {
+                return NotFound();
+            }
+
+            return View(activityType);
+        }
+
+        // POST: activityType/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var activityType = await _context.activityType.FindAsync(id);
+            if (activityType != null)
+            {
+                _context.activityType.Remove(activityType);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool activityTypeExists(int id)
+        {
+            return _context.activityType.Any(e => e.id == id);
+        }
+    }
+}
---
+++ gacha/Metadatas/activityTypeMetaData.cs
@@ -0,0 +1,19 @@
+﻿using gacha.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace gacha.Metadatas
+{
+    public class activityTypeMetaData
+    {
+        [Display(Name = "活動類別ID")]
+        public int id { get; set; }
+
+        [Display(Name = "活動名稱")]
+        [Required(ErrorMessage = "活動名稱必填")]
+        public string name { get; set; }
+
+        [Display(Name = "創建日期")]
+        //[Required(ErrorMessage = "日期必填")]
+        public DateTime? createdAt { get; set; }
+    }
+}
