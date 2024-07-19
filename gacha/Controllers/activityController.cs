2b3628410a5507790a257b6878b9b84a93a1a5b6
LiYang <liyangfullstack8776@gmail.com>
2024-07-20 01:45:07 +0800

    CREATE: Controller/ViewModel/Partial for Activity 關掉 儲值紀錄的create 關掉 edit 活動類別的創見日期
    IN PROGRESS
    Scankey chart at ExchangeRecord in progress as var exchangeRecords = Model as List<exchangeRecord> show null

---
+++ gacha/Controllers/activityController.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using gacha.Models;
+using gacha.ViewModels;
+using Microsoft.AspNetCore.Http.HttpResults;
+using System.Diagnostics;
+
+namespace gacha.Controllers
+{
+    public class activityController : Controller
+    {
+        private readonly gachaContext _context;
+
+        public activityController(gachaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: activity
+        public async Task<IActionResult> Index()
+        {
+            //var gachaContext = _context.activity.Include(a => a.activityType);
+
+            var gachaContext = _context.activity
+                .Select(activityV => new activity_ViewModel 
+                {
+                    id= activityV.id,
+                    title = activityV.title,
+                    description = activityV.description,
+                    status = activityV.status,
+                    activityTypeId = activityV.activityTypeId,
+                    createdAt = activityV.createdAt,
+                    activityStart = activityV.activityStart,
+                    activityEnd = activityV.activityEnd
+
+                });
+            return View(gachaContext);
+        }
+
+        // GET: activity/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var activity = await _context.activity
+                .Include(a => a.activityType)
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (activity == null)
+            {
+                return NotFound();
+            }
+
+            return View(activity);
+        }
+
+        // GET: activity/Create
+        public IActionResult Create()
+        {
+            ViewData["activityTypeId"] = new SelectList(_context.activityType, "id", "name");
+            return View();
+        }
+
+        // POST: activity/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("id,title,description,status,activityTypeId,createdAt,activityStart,activityEnd")] activity activity)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(activity);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["activityTypeId"] = new SelectList(_context.activityType, "id", "name", activity.activityTypeId);
+            return View(activity);
+        }
+
+        // GET: activity/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var activity = await _context.activity.FindAsync(id);
+            if (activity == null)
+            {
+                return NotFound();
+            }
+            ViewData["activityTypeId"] = new SelectList(_context.activityType, "id", "name", activity.activityTypeId);
+            return View(activity);
+        }
+
+        // POST: activity/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("id,title,description,status,activityTypeId,createdAt,activityStart,activityEnd")] activity activity)
+        {
+            if (id != activity.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(activity);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!activityExists(activity.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["activityTypeId"] = new SelectList(_context.activityType, "id", "name", activity.activityTypeId);
+            return View(activity);
+        }
+
+        // GET: activity/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var activity = await _context.activity
+                .Include(a => a.activityType)
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (activity == null)
+            {
+                return NotFound();
+            }
+
+            return View(activity);
+        }
+
+        // POST: activity/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var activity = await _context.activity.FindAsync(id);
+            if (activity != null)
+            {
+                _context.activity.Remove(activity);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool activityExists(int id)
+        {
+            return _context.activity.Any(e => e.id == id);
+        }
+    }
+}
---
+++ gacha/ViewModels/activity_ViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace gacha.ViewModels
+{
+    public class activity_ViewModel
+    {
+        [Display(Name = "活動ID")]
+        public int id { get; set; }
+
+        [Display(Name = "標題")]
+        public string title { get; set; }
+
+        [Display(Name = "描述")]
+        public string description { get; set; }
+
+        [Display(Name = "狀態")]
+        public bool status { get; set; }
+
+        [Display(Name = "活動類型ID")]
+        public int activityTypeId { get; set; }
+
+        [Display(Name = "活動創建日")]
+        public DateTime? createdAt { get; set; }
+
+        [Display(Name = "活動開始日")]
+        public DateTime? activityStart { get; set; }
+
+        [Display(Name = "活動截止日")]
+        public DateTime? activityEnd { get; set; }
+    }
+}
