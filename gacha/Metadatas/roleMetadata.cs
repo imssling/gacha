23e888beb53ba248200517db5544e6789dfe1f6b
LiYang <liyangfullstack8776@gmail.com>
2024-08-15 02:58:29 +0800

    COMPLETE
    delete @role
    create @ role

--- gacha/Metadatas/roleMetadata.cs
+++ gacha/Metadatas/roleMetadata.cs
@@ -5,8 +5,14 @@ namespace gacha.Metadatas
     public class roleMetadata
     {
         [Display(Name = "管理ID")]
+        [Required(ErrorMessage = "管理ID必填")]
+        [Range(1, int.MaxValue, ErrorMessage = "管理ID必須大於或等於1")]
         public int id { get; set; }
+
+
         [Display(Name = "管理名稱")]
+        [Required(ErrorMessage = "管理名稱必填")]
+        [StringLength(20, ErrorMessage = " 最多20個字")]
         public string title { get; set; }
     }
 }
